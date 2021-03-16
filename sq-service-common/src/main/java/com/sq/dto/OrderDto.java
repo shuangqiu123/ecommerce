0bf638aef77d599b532160f4ca895a420b3950f5
shuangqiu123 <qiushuang0625@gmail.com>
2021-03-16 17:04:29 +1100

    change auth for store management account

--- sq-service-common/src/main/java/com/sq/dto/OrderDto.java
+++ sq-service-common/src/main/java/com/sq/dto/OrderDto.java
@@ -1,2 +1,13 @@
-package com.sq.dto;public class OrderDto {
+package com.sq.dto;
+
+import com.sq.pojo.Member;
+import com.sq.pojo.Order;
+import com.sq.pojo.Payment;
+import lombok.Data;
+
+@Data
+public class OrderDto {
+    private Order order;
+    private Member member;
+    private Payment payment;
 }
