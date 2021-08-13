c4614283793d5d5c080a5ce62d9483198d5b714b
shuangqiu123 <qiushuang0625@gmail.com>
2021-08-13 14:36:29 +1000

    remove scripts

--- scripts/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-java -jar -Dspring.profiles.active=prod \
-     -Dspring.datasource.url=$URL \
-     -Dspring.datasource.username=$USERNAME \
-     -Dspring.datasource.password=$PASSWORD \
-     -Djwt.secret=$SECRETE  app.jar
