c4cbeddf1157379476d3b733d2073cab771ccec1
shuangqiu123 <qiushuang0625@gmail.com>
2021-08-13 14:28:54 +1000

    update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 java -jar -Dspring.profiles.active=prod \
-     -Dspring.datasource.url=$url \
-     -Dspring.datasource.username=$username \
-     -Dspring.datasource.password=$password \
-     -Djwt.secret=$secret  app.jar
+     -Dspring.datasource.url=$URL \
+     -Dspring.datasource.username=$USERNAME \
+     -Dspring.datasource.password=$PASSWORD \
+     -Djwt.secret=$SECRETE  app.jar
